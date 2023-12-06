fe67be8366d6767835ed4428cd17a4d36440f9a1
Abdullah Bin Mazhar <abdullahmazhar2017@gmail.com>
2023-12-06 19:51:15 +0500

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    version: "0.8.8",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+    paths: ["./contracts", "./your-library-directory"],
+  },
+}
+
